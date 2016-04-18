262f3f3203ce6cd7573257f8af8c46f0a31b2d48
u1266221 <u1266221@unimail.hud.ac.uk>
2016-04-18 02:34:02 +0100

    Added a new Techniciann Controller. Updated DAL folder.

---
+++ ITRepairDeskWebApp/Controllers/TechnicianController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ITRepairDeskWebApp.DAL;
+using ITRepairDeskWebApp.Models;
+
+namespace ITRepairDeskWebApp.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private ITRepairDeskWebAppContext db = new ITRepairDeskWebAppContext();
+
+        // GET: Technician
+        public ActionResult Index()
+        {
+            return View(db.Technicians.ToList());
+        }
+
+        // GET: Technician/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(technician);
+        }
+
+        // GET: Technician/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Technician/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TechnicianID,LastName,FirstMidName,JobAssignDate,Email,ContactNo")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Technicians.Add(technician);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(technician);
+        }
+
+        // GET: Technician/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(technician);
+        }
+
+        // POST: Technician/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TechnicianID,LastName,FirstMidName,JobAssignDate,Email,ContactNo")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(technician).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(technician);
+        }
+
+        // GET: Technician/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(technician);
+        }
+
+        // POST: Technician/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Technician technician = db.Technicians.Find(id);
+            db.Technicians.Remove(technician);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ITRepairDeskWebApp/DAL/ITRepairDeskWebAppInitializer.cs
+++ ITRepairDeskWebApp/DAL/ITRepairDeskWebAppInitializer.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
 using ITRepairDeskWebApp.Models;
 
 namespace ITRepairDeskWebApp.DAL
 {
-    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ITRepairDeskWebAppContext>
+    public class ITRepairDeskWebAppInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ITRepairDeskWebAppContext>
     {
         protected override void Seed(ITRepairDeskWebAppContext context)
         {
-            var technicians = new List<Technician>
+            var technicians= new List<Technician>
             {
-            new Technician{FirstMidName="Carson",LastName="Alexander",JobAssignDate=DateTime.Parse("2005-09-01"),Email="test@test.com",ContactNo="1234",}
+            new Technician{FirstMidName="Carson",LastName="Alexander",JobAssignDate=DateTime.Parse("2005-09-01"), Email="test@test.com", ContactNo="1234"},
+            new Technician{FirstMidName="Meredith",LastName="Alonso",JobAssignDate=DateTime.Parse("2002-09-01")},
+            new Technician{FirstMidName="Arturo",LastName="Anand",JobAssignDate=DateTime.Parse("2003-09-01")},
+            new Technician{FirstMidName="Gytis",LastName="Barzdukas",JobAssignDate=DateTime.Parse("2002-09-01")},
+            new Technician{FirstMidName="Yan",LastName="Li",JobAssignDate=DateTime.Parse("2002-09-01")},
+            new Technician{FirstMidName="Peggy",LastName="Justice",JobAssignDate=DateTime.Parse("2001-09-01")},
+            new Technician{FirstMidName="Laura",LastName="Norman",JobAssignDate=DateTime.Parse("2003-09-01")},
+            new Technician{FirstMidName="Nino",LastName="Olivetto",JobAssignDate=DateTime.Parse("2005-09-01")}
             };
 
             technicians.ForEach(s => context.Technicians.Add(s));
@@ -21,17 +27,33 @@ namespace ITRepairDeskWebApp.DAL
 
             var jobs = new List<Job>
             {
-            new Job{JobID=1050,Title="Mouse is not working",Status=Status.New,Priority=Priority.Critical,}
-
+            new Job{JobID=1050,Title="Chemistry", Status=Status.New, Priority=Priority.Critical},
+            new Job{JobID=4022,Title="Microeconomics",},
+            new Job{JobID=4041,Title="Macroeconomics",},
+            new Job{JobID=1045,Title="Calculus",},
+            new Job{JobID=3141,Title="Trigonometry",},
+            new Job{JobID=2021,Title="Composition",},
+            new Job{JobID=2042,Title="Literature",}
             };
             jobs.ForEach(s => context.Jobs.Add(s));
             context.SaveChanges();
 
-            var jobassignments = new List<JobAssignment>
+            var jobassignment = new List<JobAssignment>
             {
             new JobAssignment{TechnicianID=1,JobID=1050,},
-            };           
-            jobassignments.ForEach(s => context.JobAssignments.Add(s));
+            new JobAssignment{TechnicianID=1,JobID=4022,},
+            new JobAssignment{TechnicianID=1,JobID=4041,},
+            new JobAssignment{TechnicianID=2,JobID=1045,},
+            new JobAssignment{TechnicianID=2,JobID=3141,},
+            new JobAssignment{TechnicianID=2,JobID=2021,},
+            new JobAssignment{TechnicianID=3,JobID=1050},
+            new JobAssignment{TechnicianID=4,JobID=1050,},
+            new JobAssignment{TechnicianID=4,JobID=4022,},
+            new JobAssignment{TechnicianID=5,JobID=4041,},
+            new JobAssignment{TechnicianID=6,JobID=1045},
+            new JobAssignment{TechnicianID=7,JobID=3141,},
+            };
+            jobassignment.ForEach(s => context.JobAssignments.Add(s));
             context.SaveChanges();
         }
     }
